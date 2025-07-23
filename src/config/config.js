958a089af9af78d6b913354087d7dbc1557670b2
titan <flameplayz937@gmail.com>
2025-07-23 12:34:55 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   serverUrl: import.meta.env.VITE_APP_SERVERURL,
-  localUrl: import.meta.env.VITE_APP_LOCALURL || "http://localhost:3030/api/v1",
+  localUrl: import.meta.env.VITE_APP_LOCALURL || "https://hianime-api-55zq.onrender.com/api/v1",
   proxyUrl: import.meta.env.VITE_APP_PROXYURL,
 };
 
