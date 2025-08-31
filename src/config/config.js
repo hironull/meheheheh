5b1e220bcc4de6f993f7ffc151570058756d4705
Hiro <hiro.sys16@gmail.com>
2025-08-31 18:59:55 +0600

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   serverUrl: import.meta.env.VITE_APP_SERVERURL,
-  localUrl: import.meta.env.VITE_APP_LOCALURL || "https://hianime-api-55zq.onrender.com/api/v1",
+  localUrl: import.meta.env.VITE_APP_LOCALURL || "http://168.220.234.100:3030/api/v1",
   proxyUrl: import.meta.env.VITE_APP_PROXYURL,
   apiUrl: import.meta.env.VITE_APP_APIURL,
 };
